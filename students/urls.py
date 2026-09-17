c2f86c13eae5e8849059562a07942fbe53c055e6
Umme-Habiba123 <habibaislammim@gmail.com>
2026-09-17 20:22:12 +0600

    added crud methode

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path 
-from .views import home 
+from .views import * 
 
 urlpatterns = [
-    path('', home)
+    path('', home, name='home'),
+
+
+    path('add-student/', add_student, name='add_student'),
+    path('student/<int:id>/', student_details, name='student_details'),
+
+    
 ]
--- students/views.py
+++ students/views.py
@@ -1,10 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
-
+from .forms import StudentForm
 
 def home(request):
     students = Student.objects.all()
 
-    return render(request, 'students/index.html',{
+    return render(request, 'index.html',{
         'students' : students
     })
+
+
+def add_student(request):
+    if request.method == 'POST':
+        # name= request.POST.get('name')
+        # email=request.POST.get('email')
+        # age=request.POST.get('email')
+        form=StudentForm(request.POST)
+
+        # Student.objects.create(
+        #     name=name,
+        #     email=email,
+        #     age=age,
+
+        # )
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    else:
+            form = StudentForm()
+
+    return render(request, 'add-student.html',{
+            'form': form
+        })
+
+def student_details(request, id):
+     student=Student.objects.get(id=id)
+
+     return render(request, 'student_details.html',{
+          'student':student
+     })
+
+
+     
+        
